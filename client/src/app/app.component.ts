8eaaf43d9271fe468aaa162cb154c9881ee2bab6
anushka255 <aachary@ramapo.edu>
2022-09-15 01:19:40 -0400

    Registration and login features

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,18 @@ export class AppComponent implements OnInit{
   title = 'Hospital Management System';
   users:any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit() {
     this.getUsers();
+    this.setCurrentUser();
   }
 
+  setCurrentUser()
+  {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
   getUsers()
   {
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
